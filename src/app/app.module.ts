bb820970543c3524a9556b1768d34b229d36caaa
Roque Ayala Angel David <ingRoque98@gmail.com>
2021-09-12 21:14:32 -0500

    add bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
 
-
-// Material
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatIconModule} from '@angular/material/icon';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
-
-
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
@@ -24,14 +14,7 @@ import {MatButtonModule} from '@angular/material/button';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    FormsModule,
-    // Material
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatBadgeModule,
-    MatMenuModule,
-    MatButtonModule
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent],
